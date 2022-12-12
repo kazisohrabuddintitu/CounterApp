1fd3f718b2b2ce9f2c0392cf608826aa44c7cc94
Kazi Sohrab Uddin <kazisohrab73@gmail.com>
2022-12-12 21:35:06 +0600

    updated

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 var app=new Vue({
     el:'#app', //el refers the app id
-    
     data:{
         counter:0 //counter initially 0
+    },
+    methods:{
+
+        increment(){
+            this.counter++
+        },
+        decrement(){
+            if (this.counter<=0){  //creating condition for negative counter value
+                this.counter=0
+            }
+            else{
+                this.counter--
+            }
+        }
+
     }
 })
